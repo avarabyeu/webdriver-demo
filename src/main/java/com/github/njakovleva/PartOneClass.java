d4959a26fc6101d2d5a2a75bcc02efbe66b9e4a2
Nata-y <Nata-y@users.noreply.github.com>
2015-03-15 17:56:37 +0200

    Update PartOneClass.java
    Class renamed to SendFromGUIClass;
    getUserData() method added -  for reading user data from config.properties file;
    separate webdriver instance for every web page - implemented;
    openSenderOutbox() method added.

--- src/main/java/com/github/njakovleva/PartOneClass.java
+++ src/main/java/com/github/njakovleva/PartOneClass.java
@@ -1,55 +1,84 @@
 package Main;
 
 import ManageEmail.InboxPage;
-import ManageEmail.LoginPage;
-import ManageEmail.NewMailPage;
-import org.testng.*;
-import java.io.IOException;
-import java.util.List;
-import java.util.Properties;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-import javax.mail.Folder;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.NoSuchProviderException;
-import javax.mail.Session;
-import com.sun.mail.pop3.POP3Store;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-import com.google.common.collect.*;
+        import ManageEmail.LoginPage;
+        import ManageEmail.NewMailPage;
+        import ManageEmail.OutboxPage;
+        import Main.UserData;
+        import org.testng.*;
+        import java.io.IOException;
+        import java.util.List;
+        import java.util.Properties;
+        import java.util.Set;
+        import java.util.concurrent.TimeUnit;
+        import javax.mail.Folder;
+        import javax.mail.Message;
+        import javax.mail.MessagingException;
+        import javax.mail.NoSuchProviderException;
+        import javax.mail.Session;
+        import com.sun.mail.pop3.POP3Store;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.remote.DesiredCapabilities;
+        import org.openqa.selenium.phantomjs.PhantomJSDriver;
+        import org.testng.annotations.BeforeClass;
+        import org.testng.annotations.Test;
+        import com.google.common.collect.*;
+        import java.io.FileInputStream;
+        import java.io.IOException;
+        import java.io.InputStream;
+        import java.util.Properties;
 
 /**
- * Created by jakovleva on 10/8/2014.
+ * Created by jakovleva on 1/20/2015.
  */
-
-/**
- * Открыть в браузере почтовый ящик, создать новое письмо, отправить его.
- */
-public class PartOneClass
+public class SendFromGUIClass
 {
-    public static WebDriver driver = new PhantomJSDriver();
-    public static String senderMailUrl = "www.gmail.com";
-    public static String receiverMailUrl = "www.yandex.com";
-
+    public static String senderMailUrl;
+    public static String receiverMailUrl;
     public static String senderLogin;
     public static String senderPassword;
     public static String receiverLogin;
     public static String receiverPassword;
     public static String receiver;
-    public static String subject = "test";
+    public static String subject;
     public static String content;
 
-    public PartOneClass() {
+
+    public SendFromGUIClass() {
     }
 
-    public InboxPage sendEmail()
+    public UserData userData = new UserData();
+
+    public void sendEmail()
     {
+        getUserData();
+
+        WebDriver driver = new PhantomJSDriver();
+        LoginPage loginPage = new LoginPage(driver, senderMailUrl);
+
+        //sender actions
+        InboxPage inboxPage = loginPage.loginAs(senderLogin, senderPassword);
+        NewMailPage newMailPage = inboxPage.openComposePage();
+        inboxPage = newMailPage.sendMail(receiver, subject, content);
+    }
+
+    public InboxPage openReceiverEmail()
+    {
+        WebDriver driver = new PhantomJSDriver();
+        LoginPage loginPage = new LoginPage(driver, receiverMailUrl);
+
+        //receiver actions
+        InboxPage inboxPage = loginPage.loginAs(receiverLogin, receiverPassword);
+        return inboxPage;
+    }
+
+    public OutboxPage openSenderOutbox()
+    {
+        getUserData();
+
+        WebDriver driver = new PhantomJSDriver();
         LoginPage loginPage = new LoginPage(driver, senderMailUrl);
 
         //sender actions
@@ -57,11 +86,15 @@ public class PartOneClass
         NewMailPage newMailPage = inboxPage.openComposePage();
         inboxPage = newMailPage.sendMail(receiver, subject, content);
 
-        loginPage = inboxPage.signOut();
+        //open outbox
+        OutboxPage outboxPage = inboxPage.openOutboxPage();
 
-        //receiver actions
-        loginPage = new LoginPage(driver, receiverMailUrl);
-        inboxPage = loginPage.loginAs(receiverLogin, receiverPassword);
-        return inboxPage;
+        return outboxPage;
     }
+
+    private void getUserData() {
+        userData.getUserData(senderMailUrl, receiverMailUrl, senderLogin, senderPassword,
+                receiverLogin, receiverPassword, receiver, subject, content);
+    }
+
 }
