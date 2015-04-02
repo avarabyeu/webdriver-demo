a24e55cf16315492308ce10ac45f9e7fa1aa6800
jakovleva <natalija.yakovleva@gmail.com>
2015-04-02 12:41:42 +0300

    added methods for sending mails from different email web pages

--- src/main/java/com/github/njakovleva/SendFromGUIClass.java
+++ src/main/java/com/github/njakovleva/SendFromGUIClass.java
@@ -12,27 +12,48 @@ public class SendFromGUIClass {
 
     public void sendEmail(WebDriver driver) {
         LoginPage loginPage = new LoginPage(driver, userData.getUrlYandex());
+        //LoginPage loginPage = new LoginPage(driver, userData.getUrlInbox());
+        //LoginPage loginPage = new LoginPage(driver, userData.getUrlGmail());
+        //LoginPage loginPage = new LoginPage(driver, userData.getUrlMail());
 
         //sender actions
+
         InboxPage inboxPage = loginPage.loginAs(userData.getLoginYandex(), userData.getPasswordYandex());
         NewMailPage newMailPage = inboxPage.openComposePage();
+
+        //InboxPage inboxPage = loginPage.loginAs(userData.getLoginInbox(), userData.getPasswordInbox());
+        //NewMailPage newMailPage = inboxPage.openComposePage();
+
+        //InboxPage inboxPage = loginPage.loginAs(userData.getLoginGmail(), userData.getPasswordGmail());
+        //NewMailPage newMailPage = inboxPage.openComposePage();
+
+        //InboxPage inboxPage = loginPage.loginAs(userData.getLoginMail(), userData.getPasswordMail());
+        //NewMailPage newMailPage = inboxPage.openComposePage();
+
         inboxPage = newMailPage.sendMail(userData.getMailGmail(), userData.getSubject(), userData.getContent());
+        //inboxPage = newMailPage.sendMail(userData.getMailMail(), userData.getSubject(), userData.getContent());
     }
 
     public InboxPage openReceiverEmail(WebDriver driver) {
         LoginPage loginPage = new LoginPage(driver, userData.getUrlGmail());
 
         //receiver actions
+
         InboxPage inboxPage = loginPage.loginAs(userData.getLoginGmail(), userData.getPasswordGmail());
+        //InboxPage inboxPage = loginPage.loginAs(userData.getLoginMail(), userData.getPasswordMail());
         return inboxPage;
     }
 
     public OutboxPage openSenderOutbox(WebDriver driver) {
 
         LoginPage loginPage = new LoginPage(driver, userData.getUrlYandex());
-        //System.out.println("test");
+        //LoginPage loginPage = new LoginPage(driver, userData.getUrlInbox());
+        //LoginPage loginPage = new LoginPage(driver, userData.getUrlGmail());
+        //LoginPage loginPage = new LoginPage(driver, userData.getUrlMail());
+
+
         //sender actions
-        InboxPage inboxPage = loginPage.loginAs(userData.getLoginYandex(), userData.getPasswordYandex());
+        InboxPage inboxPage = loginPage.loginAs(userData.getLoginMail(), userData.getPasswordMail());
 
         //open outbox
         OutboxPage outboxPage = inboxPage.openOutboxPage();
